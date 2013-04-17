1fe5c17746bfe3008fb0f06f7fc9ae377944aee3
Heinz N. Gies <heinz@licenser.net>
2013-04-17 23:44:59 +0200

    Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyFi',
-    version='0.1.0',
+    version='0.1.1',
     author='Heinz N. Gies',
     author_email='heinz@licenser.net',
     packages=['fifo', 'fifo.api'],
