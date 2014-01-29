562a404cd953a1d4a9a5ca131219959ab689d9df
Heinz N. Gies <heinz@licenser.net>
2014-01-28 21:56:50 -0500

    Fixed bug in executable.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class fifo_install(install):
 
 setup(
     name='PyFi',
-    version='0.1.16',
+    version='0.1.16p1',
     author='Heinz N. Gies',
     author_email='heinz@licenser.net',
     packages=['fifo', 'fifo.api'],
