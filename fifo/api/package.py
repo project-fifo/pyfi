abc04b2b486e30cb4a5bd8f8b9961e37d68bf34f
Heinz N. Gies <heinz@licenser.net>
2014-04-01 17:42:39 +0200

    Improved package creation.

--- fifo/api/package.py
+++ fifo/api/package.py
@@ -32,20 +32,18 @@ def create(args):
     if args.quota < gb:
         msg = "The minimum amount of quote for a package is 1GB but %r byte were reqested" % args.quota
         raise argparse.ArgumentTypeError(msg)
-    payload = {
-        "name": args.name,
-        "ram": int(args.memory/mb),
-        "quota": int(args.quota/gb),
-        "cpu_cap": args.cpu_cap
-    }
-    if reqs != []:
-        payload['requirements'] = reqs
-    res = args.endpoint.create(payload)
-    if res:
-        print "Pacakge successfully created!"
+    res = args.endpoint.create(args.name, int(args.memory/mb), int(args.quota/gb), args.cpu_cap, reqs)
+    if args.p:
+        if res:
+            print res['uuid']
+        else:
+            exit(1)
     else:
-        print "Package creation failed!"
-
+        if res:
+            print "Pacakge successfully created: %s" % res['uuid']
+        else:
+            print "Package creation failed: %r" % res
+            exit(1)
 
 
 def byte_size(string):
@@ -61,7 +59,7 @@ def byte_size(string):
     p = re.compile('(\d+)(B|KB|MB|GB|TB|EB|PB)?')
     m = p.match(string)
     if not m:
-        msg = "%r is not byte size. Use <number>[B|KB|MB|GB|TB|EB|PB]" % string
+        msg = "%r is not byte size. Use <number>[B|KB|MB|GB|TB|PB|EB]" % string
         raise argparse.ArgumentTypeError(msg)
     value = int(m.group(1))
     scale = m.group(2) or 'B'
@@ -122,8 +120,16 @@ class Package(Entity):
         self._wiggle = wiggle
         self._resource = "packages"
 
-    def create(self, specs):
-        self._post(specs)
+    def create(self, name, ram, quota, cpu_cap, reqs):
+        specs = {
+            "name": name,
+            "ram": ram,
+            "quota": quota,
+            "cpu_cap": cpu_cap
+        }
+        if reqs != []:
+            specs['requirements'] = reqs
+        return self._post(specs)
 
     def make_parser(self, subparsers):
         parser_pkgs = subparsers.add_parser('packages', help='package related commands')
@@ -142,12 +148,19 @@ class Package(Entity):
         parser_pkgs_delete.add_argument("uuid")
         parser_pkgs_delete.set_defaults(func=show_get)
         parser_pkgs_create = subparsers_pkgs.add_parser('create', help='creates a new package')
-        parser_pkgs_create.add_argument("--memory", "-m", required=True, type=byte_size)
-        parser_pkgs_create.add_argument("--quota", "-q", required=True, type=byte_size)
-        parser_pkgs_create.add_argument("--cpu_cap", "-c", required=True, type=int)
-        parser_pkgs_create.add_argument("--requirement", "-r", action='append', nargs=4)
-        parser_pkgs_create.add_argument("--scale", "-s", action='append', nargs=3)
-        parser_pkgs_create.add_argument("--random", "-R", action='append', nargs=2)
-
+        parser_pkgs_create.add_argument("-p", action='store_true',
+                                        help="Output only shows the UUID no additional comments.")
+        parser_pkgs_create.add_argument("--memory", "-m", required=True, type=byte_size,
+                                        help="Amount of memory for the package.")
+        parser_pkgs_create.add_argument("--quota", "-q", required=True, type=byte_size,
+                                        help="Amount of quota for the package.")
+        parser_pkgs_create.add_argument("--cpu_cap", "-c", required=True, type=int,
+                                        help="Percentage of cores for the pacakge 100 = 1core.")
+        parser_pkgs_create.add_argument("--requirement", "-r", action='append', nargs=4,
+                                        help="Requirement rule <weight> <attr> <cond> <val>")
+        parser_pkgs_create.add_argument("--scale", "-s", action='append', nargs=3,
+                                        help="Scale rule <attr> <low> <hight>")
+        parser_pkgs_create.add_argument("--random", "-R", action='append', nargs=2,
+                                        help="Ramdom rule <low> <hight>")
         parser_pkgs_create.add_argument("name")
         parser_pkgs_create.set_defaults(func=create)
