abaea2f3420085066cbd7efa4d54ab763873ab20
Heinz N. Gies <heinz@licenser.net>
2015-11-03 05:22:52 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class fifo_install(install):
 
 setup(
     name='PyFi',
-    version='0.2.1',
+    version='0.2.2',
     author='Heinz N. Gies',
     author_email='heinz@licenser.net',
     packages=['fifo', 'fifo.api'],
