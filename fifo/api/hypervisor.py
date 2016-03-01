f9af1c5246ac3574ac92cca2edbba59414f9c779
Heinz N. Gies <heinz@licenser.net>
2016-03-01 10:08:56 -0500

    Add hypervisor stats

--- fifo/api/hypervisor.py
+++ fifo/api/hypervisor.py
@@ -32,6 +32,28 @@ def map_path(path):
     p =  path.split(":")
     return {"name": p[0], "cost": p[1]}
 
+def show_stats(args):
+    l = args.endpoint.full_list(['resources'])
+    if not l and l != []:
+        print("error!")
+        exit(1)
+    total = 0
+    used  = 0
+    reserved = 0
+    free = 0
+    for e in l:
+        total    += e['resources']['total-memory']
+        reserved += round(e['resources']['reserved-memory'])
+        used     += e['resources']['provisioned-memory']
+        free     += round(e['resources']['free-memory'])
+    if args.p:
+        print "%d:%d:%d:%d" % (total, used, reserved, free)
+        exit(0)
+    print "Total:    %15d MB" % (total)
+    print "Used:     %15d MB" % (used)
+    print "Reserved: %15d MB" % (reserved)
+    print "Free:     %15d MB" % (free)
+
 def set_path(args):
     path = map(map_path, args.path)
     res = args.endpoint.set_path(args.uuid, path)
@@ -61,7 +83,12 @@ class Hypervisor(Entity):
         parser_hypervisors_list.add_argument('--raw', '-r', action='store_true',
                             help='print json array of complete data')
         parser_hypervisors_list.set_defaults(func=show_list,
-                                          fmt_def=hypervisor_fmt)
+                                             fmt_def=hypervisor_fmt)
+
+        parser_hypervisors_stats = subparsers_hypervisors.add_parser('stats', help='stats hypervisors')
+        parser_hypervisors_stats.add_argument('-p', action='store_true')
+        parser_hypervisors_stats.set_defaults(func=show_stats)
+
         parser_hypervisors_get = subparsers_hypervisors.add_parser('get', help='gets a hypervisor')
         parser_hypervisors_get.add_argument('uuid')
         parser_hypervisors_get.set_defaults(func=show_get)
